86ab7a38ef0ddaa18c75606be6ba2587b5f64c02
Daniel Araújo <daniel.araujo@sde.ce.gov.br>
2026-03-03 12:29:47 -0300

    página como chegar

---
+++ client/src/app/components/seminario/como-chegar/como-chegar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-como-chegar',
+  standalone: false,
+  templateUrl: './como-chegar.component.html',
+  styleUrl: './como-chegar.component.css'
+})
+export class ComoChegarComponent {
+
+}
