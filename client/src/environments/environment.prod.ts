19268bfc073ed1577ea75a52c2e3a9fc5bb2a97e
Daniel Araújo <daniel.araujo@sde.ce.gov.br>
2026-03-03 13:02:39 -0300

    correção do environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8103/',
+  apiUrl: 'https://api-sisbi.sde.ce.gov.br/',
   // apiUrl: 'https://api-cotonicultura.sde.ce.gov.br/',
 };
