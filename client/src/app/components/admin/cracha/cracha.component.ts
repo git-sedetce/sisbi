25dcee231f8dd579015a98e78d42e7a6368908ca
Daniel Araújo <daniel.araujo@sde.ce.gov.br>
2026-03-03 16:10:01 -0300

    criação dos componentes

---
+++ client/src/app/components/admin/cracha/cracha.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cracha',
+  standalone: false,
+  templateUrl: './cracha.component.html',
+  styleUrl: './cracha.component.css'
+})
+export class CrachaComponent {
+
+}
