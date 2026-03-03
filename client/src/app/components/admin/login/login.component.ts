02a722df7a6b9579af02f9597813c08784ce4076
Daniel Araújo <daniel.araujo@sde.ce.gov.br>
2026-03-03 13:49:54 -0300

    página de login

---
+++ client/src/app/components/admin/login/login.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { LoginUser } from '../../../models/login-user.model';
+import { UserServiceService } from '../../../service/user-service.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+  @ViewChild("formLogin") formLogin!: NgForm;
+  loginUsers!: LoginUser;
+
+  constructor(
+    private serviceUser: UserServiceService,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit(): void {
+    this.loginUsers = new LoginUser()
+  }
+
+  verificaEmail(email: any){
+    email = this.loginUsers.user_email?.split("@")
+    //console.log('verificaEmail', email)
+    if(email[1] != 'sde.ce.gov.br'){
+      this.toastr.warning('Somente emaili @SDE!')
+      this.formLogin.reset()
+    }
+  }
+
+  login(): void {
+    this.loginUsers.user_password = this.serviceUser.CriptografarMD5(this.loginUsers.user_password)
+    // console.log('loginUser', this.loginUsers)
+    this.serviceUser.login(this.loginUsers).subscribe({
+      next: (res) => res,
+      error: (e) => (this.toastr.error(e.error.message), this.formLogin.reset())
+    })
+
+  }
+
+  gerarPin(){
+    this.serviceUser.resetPin(this.loginUsers).subscribe(
+      () =>{
+        this.toastr.success('Verifique seu Email');
+        this.formLogin.reset();
+      },
+      (error) => {
+        this.toastr.error('Erro durante o processo', error.error.message);
+        this.formLogin.reset();
+      }
+    );
+
+  }
+
+}
